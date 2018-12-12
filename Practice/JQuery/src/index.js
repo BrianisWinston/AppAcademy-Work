f842842dc5fbc0ec38be7cec844ba757dc2f0029
Winston Galas <BrianisWinston@yahoo.com>
2018-12-12 15:27:06 -0800

    Add if logic

--- Practice/JQuery/src/index.js
+++ Practice/JQuery/src/index.js
@@ -1,6 +1,11 @@
 const DOMNodeCollections = require("./dom_node_collection.js");
 
 window.$l = function (hello) {
-  let elements = document.querySelectorAll(`${hello}`);
-  return elements;
+  if (hello instanceof HTMLElement) {
+
+  } else {
+    let elements = document.querySelectorAll(`${hello}`);
+    let newEls = new DOMNodeCollections(elements);
+    return newEls;
+  }
 };
